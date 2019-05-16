2e77fdf8b480b62e8b3183fead21d4e6c81515d5
Harsh Anand <44001232+harsh-not-haarsh@users.noreply.github.com>
2019-05-16 14:09:34 +0530

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -2,6 +2,8 @@
 ECHO.
 ECHO This is a programme that will install all the necessary libraries required
 pip install numpy
+pip install opencv-python
+pip install skimage
 pip install tensorflow
 pip install keras
 pip install theano
