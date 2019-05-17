738be55cee40ac21421280c3c8657430c8808b54
Harsh Anand <44001232+harsh-not-haarsh@users.noreply.github.com>
2019-05-17 13:49:59 +0530

    Add files via upload

--- track.py
+++ track.py
@@ -3,6 +3,11 @@ import math
 import os
 import numpy as np
 import time
+
+# This function is the heart of the code, Given the pupil images as input it will
+# multiply the grayscale image of the pupil to a matrix (convulation) and then compare the product to 
+# a matrix of zeros, using this the approximate position of the pupil will be determined
+
 def track(img):
 	n=3
 	var=(n//2) +1
@@ -29,36 +34,40 @@ def track(img):
 	else:
 		return([X//count,Y//count])
 
+# Cuts the upper area of the face i.e. the area, eyes are most expected to be
+# hence reducing the probability of identifying the chin (or something) as a false eye
+
 def filter_face(img,gray_img,face):
 
 	return (img[face[0]:face[0]+int(6*face[2]/8),face[1]:face[1]+face[3]],
 			gray_img[face[0]:face[0]+int(6*face[2]/8),face[1]:face[1]+face[3]])
 
 
+# removing the eyebrows so as to increase the accuracy of detecting pupils (in the grayscale image)
 
 def remove_brows(img):
 	hig=len(img)
 	wid=len(img[0])
 	return img[int(0.12*hig):,int(0.6*wid):int(0.94*wid)]
 
-def face_bb(img,face):
+def face_bb(img,face):# creates a bounding box around the face
 	cv2.rectangle(img,(face[0],face[1]),(face[0]+face[2],face[1]+face[3]),(123,20,12),2)
 
-
+# Calculating the number of eyes detected and taking wrt to the position
 def draw(eyes,images):
-	#Yavg=int((eyes[0][1]+eyes[1][1])/2)
-	#print(Yavg)
 	n=len(eyes)
 	if n < 2:
-		print("machine")
+		print("Machine") # Call the function to run the car via the algorithm
+	elif n==2:
+		print("Human")
 	else:
-		print("human")
+		print("Alien")  # I have no idea what to do here
 	for i in range(0,n):
 		cv2.circle(images[i],(eyes[i][0],eyes[i][1]),5,(0,0,150),-1)
 
+# Preprocessing the image of the eye, pupil is to be extracted from 
 def refine(img,TH):
 	img =cv2.medianBlur(img, 5)
-	
 	_,img = cv2.threshold(img, TH,255, cv2.THRESH_BINARY)
 	img =cv2.erode(img,None,iterations=2)
 	img =cv2.dilate(img,None,iterations=4)
@@ -76,14 +85,18 @@ cv2.namedWindow("Image")
 Threshold=69
 while True:
 	_,image=cap.read()
+
 	# Reading the image
 	#image=cv2.imread("HARSH.jpg")
 	#image=cv2.imread("test_4.jpg")
+	
 	gray_image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) # will convert the image to greyscale (obviously)
+	
 	# this tells the faces in the image, first argument is the gray scale image,
 	# second is the reduction in size for each frame/ image and third tells the min neighbours each rectangle should
 	# have to retain it
 	faces=face_cascade.detectMultiScale(gray_image,1.3,5)
+	
 	#this draws the rectangle aroung the face
 	gray_faces=[]
 	face_img=[]
@@ -91,11 +104,11 @@ while True:
 	eye_images=[]
 	for face in faces:
 		face_bb(image,face)
-		face_img,gray_face=filter_face(image,gray_image,face)
-		eyes=eye_cascade.detectMultiScale(face_img,1.3,5)
+		face_img,gray_face=filter_face(image,gray_image,face) # detcts the face using haar cascade dataset
+		eyes=eye_cascade.detectMultiScale(face_img,1.3,5) #detects the eyes using haar cascade dataset
 		i=0
 		for eye in eyes:
-			cv2.rectangle(face_img,(eye[0],eye[1]),(eye[0]+eye[2],eye[1]+eye[3]),(0,255,0),1)
+			cv2.rectangle(face_img,(eye[0],eye[1]),(eye[0]+eye[2],eye[1]+eye[3]),(0,255,0),1) #drawing a BB box around the eyes
 			eye_image=face_img[eye[1]:eye[1]+int(eye[3]),eye[0]:eye[0]+int(eye[2])]
 			gray_eye_image=gray_face[eye[1]:eye[1]+int(eye[3]),eye[0]:eye[0]+int(eye[2])]
 			gray_eye_image =refine(gray_eye_image,Threshold)
